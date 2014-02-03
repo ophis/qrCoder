135d3a8e529edcfab24291146abc34bb18662287
Frank <Frank@Ophis-PC>
2014-02-03 14:12:48 -0500

    update

--- src/textProcessor/enCoder.java
+++ src/textProcessor/enCoder.java
@@ -1,7 +1,30 @@
 package textProcessor;
 
 public class enCoder {
-	public String byte2String(byte[] bytes){
-		return "";
+	public static String byte2hex(byte[] b) {
+		// 一个字节的数，
+		// 转成16进制字符串
+		String hs = "";
+		String stmp = "";
+		for (int n = 0; n < b.length; n++) {
+			//int to hex
+			stmp = (java.lang.Integer.toHexString(b[n] & 0XFF));
+			if (stmp.length() == 1)
+				hs = hs + "0" + stmp;
+			else
+				hs = hs + stmp;
+		}
+		return hs.toUpperCase(); //to upper case
+	}
+
+	public static byte[] hex2byte(byte[] b) {
+		if ((b.length % 2) != 0)
+			throw new IllegalArgumentException("lenth is not even");
+		byte[] b2 = new byte[b.length / 2];
+		for (int n = 0; n < b.length; n += 2) {
+			String item = new String(b, n, 2);
+			b2[n / 2] = (byte) Integer.parseInt(item, 16);
+		}
+		return b2;
 	}
 }
